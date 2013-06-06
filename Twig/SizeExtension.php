f80e4f9bdbcf7797d716dc90b17efa4a35d1286f
lroth <lukasz.roth@gmail.com>
2013-06-06 17:24:31 +0200

    file size in human readable format

---
+++ Twig/SizeExtension.php
@@ -0,0 +1,53 @@
+<?php
+namespace Btn\BaseBundle\Twig;
+
+class SizeExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('size', array($this, 'sizeFilter')),
+        );
+    }
+
+    public function sizeFilter($number)
+    {
+
+        return $this->formatSizeUnits($number);
+    }
+
+    private function formatSizeUnits($bytes)
+    {
+        if ($bytes >= 1073741824)
+        {
+            $bytes = number_format($bytes / 1073741824, 2) . ' GB';
+        }
+        elseif ($bytes >= 1048576)
+        {
+            $bytes = number_format($bytes / 1048576, 2) . ' MB';
+        }
+        elseif ($bytes >= 1024)
+        {
+            $bytes = number_format($bytes / 1024, 2) . ' KB';
+        }
+        elseif ($bytes > 1)
+        {
+            $bytes = $bytes . ' bytes';
+        }
+        elseif ($bytes == 1)
+        {
+            $bytes = $bytes . ' byte';
+        }
+        else
+        {
+            $bytes = '0 bytes';
+        }
+
+        return $bytes;
+    }
+
+    public function getName()
+    {
+        return 'size_extension';
+    }
+}
