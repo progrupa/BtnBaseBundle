d4ab0968d4373a2ab536946dddd1e6f2a5dcc037
Michał Soczyński <soczynski.michal@gmail.com>
2014-04-10 16:51:33 +0200

    add doctrine listener for collate and charset

---
+++ Listener/DoctrineEvent.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Btn\BaseBundle\Listener;
+
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\ORM\Mapping\Table;
+
+class DoctrineEvent
+{
+    /**
+     * standard options array from parameters
+     *
+     * @var array
+     **/
+    private $options;
+
+    /**
+     * standard construct
+     *
+     * @return void
+     **/
+    public function __construct($options)
+    {
+        if (!$options) {
+            $options = array();
+        }
+
+        $this->options = array_merge(array('collate' => 'utf8_general_ci', 'charset' => 'utf8'), $options);
+    }
+
+    public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
+    {
+        $reader      = new AnnotationReader;
+        $metadata    = $eventArgs->getClassMetadata();
+        $class       = $metadata->getReflectionClass();
+        $annotations = $reader->getClassAnnotations($class);
+
+        foreach ($annotations as $annotation) {
+            if ($annotation instanceof Table) {
+                $metadata->table['options'] = $this->options;
+            }
+        }
+    }
+}
