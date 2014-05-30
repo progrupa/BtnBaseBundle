4953aa5d0b3f94d0c4c9a663a7d88eba04b1f017
Adam Misiorny <adam.misiorny@gmail.com>
2014-05-30 16:44:11 +0200

    add controller method and twig extension for easier csrf handle in links
    Conflicts:
    	Controller/BaseController.php
    	Resources/config/services.yml

---
+++ Twig/CsrfPathExtension.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Btn\BaseBundle\Twig;
+
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Form\Extension\Csrf\CsrfProvider\CsrfProviderInterface;
+
+/**
+ *
+ */
+class CsrfPathExtension extends \Twig_Extension
+{
+    /** @var \Symfony\Component\Routing\UrlGeneratorInterface */
+    protected $router;
+    /** @var \Symfony\Component\Form\Extension\Csrf\CsrfProvider\CsrfProviderInterface */
+    protected $csrfProvider;
+
+    /**
+     *
+     */
+    public function __construct(UrlGeneratorInterface $router, CsrfProviderInterface $csrfProvider)
+    {
+        $this->router       = $router;
+        $this->csrfProvider = $csrfProvider;
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            'csrf_path' => new \Twig_Function_Method($this, 'csrfPath'),
+        );
+    }
+
+    public function csrfPath($name, $parameters = array(), $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH)
+    {
+        if (empty($parameters['csrf_token'])) {
+            $parameters['csrf_token'] = $this->csrfProvider->generateCsrfToken($name);
+        }
+
+        return $this->router->generate($name, $parameters, $referenceType);
+    }
+
+    public function getName()
+    {
+        return 'btn_csrf_path_extension';
+    }
+}
