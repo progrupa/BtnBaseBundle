5d482ccd5e6e4262f691ff1c1dd439985831ad8b
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-17 00:15:13 +0200

    add bundle helper

---
+++ Helper/BundleHelper.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Btn\BaseBundle\Helper;
+
+use Symfony\Component\DependencyInjection\Container;
+
+class BundleHelper
+{
+    /** @var \ReflectionClass[] $reflectionClasses */
+    protected $reflectionClasses = array();
+
+    /**
+     *
+     */
+    public function getReflectionClass($class)
+    {
+        $hash = spl_object_hash($class);
+        if (!isset($this->reflectionClasses[$hash])) {
+            $this->reflectionClass[$hash] = new \ReflectionClass($class);
+        }
+
+        return $this->reflectionClass[$hash];
+    }
+
+    /**
+     *
+     */
+    public function getClassName($class)
+    {
+        if (is_object($class)) {
+            $class = get_class($class);
+        }
+
+        return $class;
+    }
+
+    /**
+     *
+     */
+    public function getBundleName($class)
+    {
+        $className = $this->getClassName($class);
+
+        if (preg_match(('~[A-Za-z\\\\0-9]+Bundle~'), $className, $matches)) {
+            return str_replace('\\', '', $matches[0]);
+        }
+
+        throw new \Exception(sprintf('Could not get bundle name from "%s"', $className));
+    }
+
+    /**
+     *
+     */
+    public function getBundleAlias($class)
+    {
+        $bn = $this->getBundleName($class);
+
+        return Container::underscore(substr($bn, 0, -6));
+    }
+
+    /**
+     *
+     */
+    public function getControllerName($class)
+    {
+        $className = $this->getClassName($class);
+
+        if (preg_match(('~\\\\([A-Za-z0-9]+)Controller$~'), $className, $matches)) {
+            return $matches[1];
+        }
+
+        throw new \Exception(sprintf('Could not get controller name from "%s"', $className));
+    }
+
+    /**
+     *
+     */
+    public function getTemplatePrefix($class)
+    {
+        $className      = $this->getClassName($class);
+        $bundleName     = $this->getBundleName($className);
+        $controllerName = $this->getControllerName($className);
+
+        return $bundleName . ':' . $controllerName . ':';
+    }
+
+    /**
+     *
+     */
+    public function getProviderId($class)
+    {
+        $className = $this->getClassName($class);
+
+        if (preg_match(('~\\\\([A-Za-z0-9]+)ControlController$~'), $className, $matches)) {
+            return $this->getBundleAlias($class) . '.provider.' . Container::underscore($matches[1]);
+        }
+
+        throw new \Exception(sprintf('Could not get provider id from "%s"', $className));
+    }
+
+    /**
+     *
+     */
+    public function getFormId($class)
+    {
+        $bundleAlias    = $this->getBundleAlias($class);
+        $controllerName = $this->getControllerName($class);
+
+        return $bundleAlias . '.form.' . Container::underscore($controllerName);
+    }
+}
