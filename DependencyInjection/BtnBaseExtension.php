23c8e49f508d6546069ccf8fce098a946170cdfa
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-16 18:08:20 +0200

    use abstract extension

--- DependencyInjection/BtnBaseExtension.php
+++ DependencyInjection/BtnBaseExtension.php
@@ -3,34 +3,25 @@
 namespace Btn\BaseBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 
 /**
- * This is the class that loads and manages your bundle configuration
  *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class BtnBaseExtension extends Extension
+class BtnBaseExtension extends AbstractExtension
 {
     /**
      * {@inheritDoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        parent::load($configs, $container);
 
-        $container->setParameter('btn_base', array());
+        $config = $this->getProcessedConfig($container, $configs);
 
         $container->setParameter('btn_base.livereload.enabled', $config['livereload']['enabled']);
         $container->setParameter('btn_base.livereload.host', $config['livereload']['host']);
         $container->setParameter('btn_base.livereload.port', $config['livereload']['port']);
         $container->setParameter('btn_base.doctrine.table.options', $config['doctrine']['table']['options']);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('parameters.yml');
-        $loader->load('services.yml');
     }
 }
