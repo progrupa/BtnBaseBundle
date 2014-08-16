abc752b6d096128a09412a3dcd7aaf36d9d78458
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-16 21:25:56 +0200

    add methods to AssetLoaderInterface

--- Assetic/Loader/AssetLoaderInterface.php
+++ Assetic/Loader/AssetLoaderInterface.php
@@ -4,4 +4,7 @@ namespace Btn\BaseBundle\Assetic\Loader;
 
 interface AssetLoaderInterface
 {
+    public function requset($group, $asset);
+    public function load($assets);
+    public function has($asset);
 }
