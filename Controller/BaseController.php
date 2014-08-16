9df6c637e1f3a3b0f97200bbf30b8b1a826df461
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-16 10:29:11 +0200

    add depricated message for BaseController

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -2,6 +2,16 @@
 
 namespace Btn\BaseBundle\Controller;
 
+/**
+ * @deprecated Deprecated in favore of AbstractController
+ */
 class BaseController extends AbstractController
 {
+    public function __construct()
+    {
+        trigger_error(
+            'BaseController is deprecated. Use AbstractController.',
+            E_USER_DEPRECATED
+        );
+    }
 }
