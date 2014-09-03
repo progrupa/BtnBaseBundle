c609c5a9e8eff1119d8e25acc6a9220fc80b3dd5
Lesyek <leszek@bitnoi.se>
2014-09-03 15:55:18 +0200

    fix

--- src/Btn/BaseBundle/Filter/BaseFilter.php
+++ src/Btn/BaseBundle/Filter/BaseFilter.php
@@ -12,114 +12,114 @@ use Doctrine\ORM\EntityRepository;
  */
 class BaseFilter
 {
-    /**
-     * @var EntityRepository
-     */
-    protected $repo;
+    /**
+     * @var EntityRepository
+     */
+    protected $repo;
 
-    /**
-     * @var Request
-     */
-    protected $request = null;
+    /**
+     * @var Request
+     */
+    protected $request = null;
 
-    /**
-     * @var QueryBuilder
-     */
-    protected $qb;
+    /**
+     * @var QueryBuilder
+     */
+    protected $qb;
 
-    /**
-     * @var string
-     */
-    protected $bundleNamespace;
+    /**
+     * @var string
+     */
+    protected $bundleNamespace;
 
-    /**
-     * @var string
-     */
-    protected $entityName;
+    /**
+     * @var string
+     */
+    protected $entityName;
 
-    /**
-     * @var string
-     */
-    protected $entityNamespace;
+    /**
+     * @var string
+     */
+    protected $entityNamespace;
 
-    /**
-     * @var array
-     */
-    protected $query = array();
+    /**
+     * @var array
+     */
+    protected $query = array();
 
-    /**
-     * @var EntityManager
-     */
-    protected $em;
+    /**
+     * @var EntityManager
+     */
+    protected $em;
 
-    /**
-     * Set repository
-     *
-     * @param EntityManager $em
-     * @param string $bundleNamespace
-     * @param string $entityName
-     */
-    public function __construct(EntityManager $em, $bundleNamespace, $entityName)
-    {
-        $this->bundleNamespace = $bundleNamespace;
-        $this->entityNamespace = $bundleNamespace . '\Entity\\' . $entityName;
-        $this->entityName      = $entityName;
-        $this->repo            = $em->getRepository($bundleNamespace . '\Entity\\' . $entityName);
-        $this->em              = $em;
-    }
+    /**
+     * Set repository
+     *
+     * @param EntityManager $em
+     * @param string $bundleNamespace
+     * @param string $entityName
+     */
+    public function __construct(EntityManager $em, $bundleNamespace, $entityName)
+    {
+        $this->bundleNamespace = $bundleNamespace;
+        $this->entityNamespace = $bundleNamespace . '\Entity\\' . $entityName;
+        $this->entityName      = $entityName;
+        $this->repo            = $em->getRepository($bundleNamespace . '\Entity\\' . $entityName);
+        $this->em              = $em;
+    }
 
-    /**
-     * @param Request $request
-     */
-    public function setRequest(Request $request)
-    {
-        $this->request = $request;
-        $this->query   = array_merge($request->query->all(), $request->request->all());
-    }
+    /**
+     * @param Request $request
+     */
+    public function setRequest(Request $request)
+    {
+        $this->request = $request;
+        $this->query   = array_merge($request->query->all(), $request->request->all());
+    }
 
-    /**
-     * get query string
-     *
-     * @return mixed
-     **/
-    public function getQuery($string)
-    {
-        if (isset($this->query[$string])) {
-            return $this->query[$string];
-        }
+    /**
+     * get query string
+     *
+     * @return mixed
+     **/
+    public function getQuery($string)
+    {
+        if (isset($this->query[$string])) {
+            return $this->query[$string];
+        }
 
-        return $this->request->get($string);
-    }
+        return $this->request->get($string);
+    }
 
-    /**
-     * @return QueryBuilder
-     */
-    public function getQueryBuilder()
-    {
-        return $this->qb;
-    }
+    /**
+     * @return QueryBuilder
+     */
+    public function getQueryBuilder()
+    {
+        return $this->qb;
+    }
 
-    /**
-     * @return array
-     */
-    public function getResult()
-    {
-        return $this->qb->getQuery()->getResult();
-    }
+    /**
+     * @return array
+     */
+    public function getResult()
+    {
+        return $this->qb->getQuery()->getResult();
+    }
 
-    /**
-     * Bind entity fields from array
-     *
-     * @return void
-     **/
-    public function exprEntityFields($fields, $alias)
-    {
-        foreach ($fields as $field) {
-            if ($value = $this->getQuery($field)) {
-                $this->qb
-                    ->andWhere($alias . '.' . $field . ' = :' . $field)
-                    ->setParameter($field, $value);
-            }
-        }
-    }
+    /**
+     * Bind entity fields from array
+     *
+     * @return void
+     **/
+    public function exprEntityFields($fields, $alias)
+    {
+        foreach ($fields as $field) {
+            if ($value = $this->getQuery($field)) {
+                $this->qb
+                    ->andWhere($alias . '.' . $field . ' = :' . $field)
+                    ->setParameter($field, $value);
+            }
+        }
+    }
 }
