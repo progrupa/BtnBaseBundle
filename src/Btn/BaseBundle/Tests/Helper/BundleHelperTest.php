32e2297bf96d49c1894a82a8333a86d3d6d6be15
Adam Misiorny <adam.misiorny@gmail.com>
2014-11-02 15:55:19 +0100

    finish tests for bundle helper

--- src/Btn/BaseBundle/Tests/Helper/BundleHelperTest.php
+++ src/Btn/BaseBundle/Tests/Helper/BundleHelperTest.php
@@ -41,30 +41,223 @@ class BundleHelperTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     *
+     * @dataProvider getFixtures
      */
-    public function testGetClassName()
+    public function testGetClassName(array $input)
     {
-        $controllerClassName    = $this->helper->getClassName($this->controller);
-        $redControllerClassName = $this->helper->getClassName($this->redirectController);
+        if (isset($input['className'])) {
+            $className = $this->helper->getClassName($input['class']);
+            $this->assertEquals($input['className'], $className);
+        }
+    }
 
-        $this->assertEquals('Symfony\\Bundle\\FrameworkBundle\\Controller\\Controller', $controllerClassName);
-        $this->assertEquals('Btn\\BaseBundle\\Controller\\RedirectingController', $redControllerClassName);
+    /**
+     * @dataProvider getFixtures
+     */
+    public function testGetBundlePath(array $input)
+    {
+        if (isset($input['bundlePath'])) {
+            $bundlePath = $this->helper->getBundlePath($input['class']);
+            $this->assertEquals($input['bundlePath'], $bundlePath);
+        }
     }
 
     /**
      *
      */
-    public function testGetBundleName()
+    public function testGetBundlePathException()
     {
-        $simpleBudleName         = $this->helper->getBundleName('Btn\\Some\\Dir\\SimpleBundle');
-        $camelCaseBundleName     = $this->helper->getBundleName('Btn\\Some\\Dir\\CamelCaseBundle');
-        $redControllerBundleName = $this->helper->getBundleName($this->redirectController);
-        $controllerBundleName    = $this->helper->getBundleName($this->controller);
+        try {
+            $this->helper->getBundlePath('SomeRandomName');
+            $this->assertFalse(true, 'Should throw exception');
+        } catch (\Exception $e) {
+            $this->assertTrue(true);
+        }
+    }
 
-        $this->assertEquals('BtnSomeDirSimpleBundle', $simpleBudleName);
-        $this->assertEquals('BtnSomeDirCamelCaseBundle', $camelCaseBundleName);
-        $this->assertEquals('BtnBaseBundle', $redControllerBundleName);
-        $this->assertEquals('FrameworkBundle', $controllerBundleName);
+
+    /**
+     * @dataProvider getFixtures
+     */
+    public function testGetBundleName(array $input)
+    {
+        if (isset($input['bundleName'])) {
+            $bundleName = $this->helper->getBundleName($input['class']);
+            $this->assertEquals($input['bundleName'], $bundleName);
+        }
+    }
+
+    /**
+     *
+     */
+    public function testGetBundleNameException()
+    {
+        try {
+            $this->helper->getBundleName('SomeRandomName');
+            $this->assertFalse(true, 'Should throw exception');
+        } catch (\Exception $e) {
+            $this->assertTrue(true);
+        }
+    }
+
+    /**
+     * @dataProvider getFixtures
+     */
+    public function testGetBundleAlias(array $input)
+    {
+        if (isset($input['bundleAlias'])) {
+            $bundleAlias = $this->helper->getBundleAlias($input['class']);
+            $this->assertEquals($input['bundleAlias'], $bundleAlias);
+        }
+    }
+
+    /**
+     * @dataProvider getFixtures
+     */
+    public function testGetControllerName(array $input)
+    {
+        if (isset($input['controllerName'])) {
+            $controllerName = $this->helper->getControllerName($input['class']);
+            $this->assertEquals($input['controllerName'], $controllerName);
+        }
+    }
+
+    /**
+     * @dataProvider getFixtures
+     */
+    public function testGetUnderscoreControllerName(array $input)
+    {
+        if (isset($input['controller_name'])) {
+            $controller_name = $this->helper->getUnderscoreControllerName($input['class']);
+            $this->assertEquals($input['controller_name'], $controller_name);
+        }
+    }
+
+    /**
+     * @dataProvider getFixtures
+     */
+    public function testGetTemplatePrefix(array $input)
+    {
+        if (isset($input['templatePrefix'])) {
+            $templatePrefix = $this->helper->getTemplatePrefix($input['class']);
+            $this->assertEquals($input['templatePrefix'], $templatePrefix);
+        }
+    }
+
+    /**
+     * @dataProvider getFixtures
+     */
+    public function testGetRoutePrefix(array $input)
+    {
+        if (isset($input['routePrefix'])) {
+            $routePrefix = $this->helper->getRoutePrefix($input['class']);
+            $this->assertEquals($input['routePrefix'], $routePrefix);
+        }
+    }
+
+    /**
+     * @dataProvider getFixtures
+     */
+    public function testGetProviderId(array $input)
+    {
+        if (isset($input['providerId'])) {
+            $providerId = $this->helper->getProviderId($input['class']);
+            $this->assertEquals($input['providerId'], $providerId);
+        }
+    }
+
+    /**
+     * @dataProvider getFixtures
+     */
+    public function testGetFormId(array $input)
+    {
+        if (isset($input['formId'])) {
+            $formId = $this->helper->getFormId($input['class']);
+            $this->assertEquals($input['formId'], $formId);
+        }
+    }
+
+    /**
+     * @dataProvider getFixtures
+     */
+    public function testGetFormAlias(array $input)
+    {
+        if (isset($input['formAlias'])) {
+            $formAlias = $this->helper->getFormAlias($input['class']);
+            $this->assertEquals($input['formAlias'], $formAlias);
+        }
+    }
+
+    /**
+     *
+     */
+    public function getFixtures()
+    {
+        return array(
+            array(
+                array(
+                    'class'           => new RedirectingController(),
+                    'className'       => 'Btn\\BaseBundle\\Controller\\RedirectingController',
+                    'bundlePath'      => 'Btn\\BaseBundle',
+                    'bundleName'      => 'BtnBaseBundle',
+                    'bundleAlias'     => 'btn_base',
+                    'controllerName'  => 'Redirecting',
+                    'controller_name' => 'redirecting',
+                    'templatePrefix'  => 'BtnBaseBundle:Redirecting:',
+                    'routePrefix'     => 'btn_base_redirecting',
+                ),
+            ),
+            array(
+                array(
+                    'class'           => new Controller(),
+                    'className'       => 'Symfony\\Bundle\\FrameworkBundle\\Controller\\Controller',
+                    'bundlePath'      => 'Symfony\\Bundle\\FrameworkBundle',
+                    'bundleName'      => 'FrameworkBundle',
+                    'bundleAlias'     => 'framework',
+                ),
+            ),
+            array(
+                array(
+                    'class'           => 'Symfony\\Bundle\\TwigBundle\\Controller\\ExceptionController',
+                    'className'       => 'Symfony\\Bundle\\TwigBundle\\Controller\\ExceptionController',
+                    'bundlePath'      => 'Symfony\\Bundle\\TwigBundle',
+                    'bundleName'      => 'TwigBundle',
+                    'bundleAlias'     => 'twig',
+                    'controllerName'  => 'Exception',
+                    'controller_name' => 'exception',
+                    'templatePrefix'  => 'TwigBundle:Exception:',
+                    'routePrefix'     => 'twig_exception',
+                ),
+            ),
+            array(
+                array(
+                    'class'           => 'Btn\\Some\\Dir\\SimpleBundle\\Controller\\SomeController',
+                    'className'       => 'Btn\\Some\\Dir\\SimpleBundle\\Controller\\SomeController',
+                    'bundlePath'      => 'Btn\\Some\\Dir\\SimpleBundle',
+                    'bundleName'      => 'BtnSomeDirSimpleBundle',
+                    'bundleAlias'     => 'btn_some_dir_simple',
+                    'controllerName'  => 'Some',
+                    'controller_name' => 'some',
+                    'templatePrefix'  => 'BtnSomeDirSimpleBundle:Some:',
+                    'routePrefix'     => 'btn_some_dir_simple_some',
+                ),
+            ),
+            array(
+                array(
+                    'class'           => 'Btn\\Some\\Dir\\CamelCaseBundle\\Controller\\SomeControlController',
+                    'className'       => 'Btn\\Some\\Dir\\CamelCaseBundle\\Controller\\SomeControlController',
+                    'bundlePath'      => 'Btn\\Some\Dir\\CamelCaseBundle',
+                    'bundleName'      => 'BtnSomeDirCamelCaseBundle',
+                    'bundleAlias'     => 'btn_some_dir_camelcase',
+                    'controllerName'  => 'SomeControl',
+                    'controller_name' => 'some_control',
+                    'templatePrefix'  => 'BtnSomeDirCamelCaseBundle:SomeControl:',
+                    'routePrefix'     => 'btn_some_dir_camelcase_somecontrol',
+                    'providerId'      => 'btn_some_dir_camelcase.provider.some',
+                    'formId'          => 'btn_some_dir_camelcase.form.some_control',
+                    'formAlias'       => 'btn_some_dir_camelcase_form_some_control',
+                ),
+            ),
+        );
     }
 }
